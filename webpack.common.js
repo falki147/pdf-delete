47135ccea4000edd50e7e2338eafcde40d421d1b
Florian Preinfalk <preinfalkflorian@gmail.com>
2020-10-08 20:40:47 +0200

    Add production config

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    context: __dirname,
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'assets' }
+            ]
+        })
+    ],
+    entry: {
+        "main": "./src/index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [ "style-loader", "css-loader", "sass-loader" ]
+            },
+            {
+                test: /\.svg$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].bundle.js",
+    }
+};
